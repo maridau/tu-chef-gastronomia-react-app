3eefd7b5ffc14433377c9916e349bab4d956cad5
maridau <marina.dau@gmail.com>
2019-07-02 19:21:36 -0300

    comienzo de register

---
+++ src/Experience.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './experience.css';
+
+class Experience extends React.Component {
+    render() {
+
+        return (
+            <div className="container">
+                <h1>RESERVA TU EXPERIENCIA</h1>
+                <h2>Define los detalles de tu solicitud y nuestros Chefs te enviarán un menú a medida.</h2>
+            </div>
+
+        );
+    }
+}
+
+export default Experience;
