76a9dfdb92602babfd6675c344c4dc3167e55979
maridau <marina.dau@gmail.com>
2019-07-13 22:04:33 -0300

    ajuste de margen

--- src/Event/EventResume.js
+++ src/Event/EventResume.js
@@ -20,7 +20,7 @@ const styles = theme => ({
     },
     card: {
         maxWidth: 400,
-        margin: "auto",
+        margin: theme.spacing.unit * 3,
         transition: "0.3s",
         boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
         "&:hover": {
